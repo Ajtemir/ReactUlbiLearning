a11c9552586372f8ab1305daff903f68b042b8d3
aulanbekuulu <aulanbekuulu@srs.lan>
2023-09-28 16:39:25 +0600

    class driven component is legacy, work with binding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
     const [value, setValue] = useState('Текст в инпуте')
   return (
     <div className="App">
-        <Counter/>
+        <ClassCounter/>
     </div>
   );
 }
