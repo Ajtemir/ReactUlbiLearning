d1218ade518bf3aa3365dcb9223aec8e31337b13
aulanbekuulu <aulanbekuulu@srs.lan>
2023-09-29 11:22:44 +0600

    Посты про 2 языка программирования

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, {useState} from 'react';
 import './styles/App.css';
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 function App() {
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'Javascript', body: 'Description'},
+        { id: 2, title: 'Javascript 2', body: 'Description 2'},
+        { id: 3, title: 'Javascript 3', body: 'Description 3'},
+    ]);
+
+    const [posts2, setPosts2] = useState([
+        { id: 1, title: 'Python', body: 'Description'},
+        { id: 2, title: 'Python 2', body: 'Description 2'},
+        { id: 3, title: 'Python 3', body: 'Description 3'},
+    ]);
   return (
     <div className="App">
-        <PostItem post={{ id: 1, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{ id: 2, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{ id: 3, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{ id: 4, title: 'Javascript', body: 'Description'}} />
-        <PostItem post={{ id: 5, title: 'Javascript', body: 'Description'}} />
+        <PostList posts={posts} title='Посты про JS'/>
+        <PostList posts={posts} title='Посты про Python'/>
     </div>
   );
 }
