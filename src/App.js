ed29bc10adf9103a8279ce7fdcfea81a6d2367b2
aulanbekuulu <aulanbekuulu@srs.lan>
2023-09-29 11:37:34 +0600

    из-за деструктизации все будет улетать правильно

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 function App() {
     const [posts, setPosts] = useState([
         { id: 1, title: 'Javascript', body: 'Description'},
@@ -9,15 +10,14 @@ function App() {
         { id: 3, title: 'Javascript 3', body: 'Description 3'},
     ]);
 
-    const [posts2, setPosts2] = useState([
-        { id: 1, title: 'Python', body: 'Description'},
-        { id: 2, title: 'Python 2', body: 'Description 2'},
-        { id: 3, title: 'Python 3', body: 'Description 3'},
-    ]);
   return (
     <div className="App">
+        <form action="">
+            <input type="text" placeholder="Название поста"/>
+            <input type="text" placeholder="Описание поста"/>
+            <MyButton disabled>Создать пост</MyButton>
+        </form>
         <PostList posts={posts} title='Посты про JS'/>
-        <PostList posts={posts} title='Посты про Python'/>
     </div>
   );
 }
