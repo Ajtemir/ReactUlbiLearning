6ff63d5d5ee5a8466589699aa64fa9b3eb30b477
aulanbekuulu <aulanbekuulu@srs.lan>
2023-10-02 11:14:59 +0600

    product added and ordered by index

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -8,7 +8,9 @@ const PostList = ({posts, title}) => {
                 {title}
             </h1>
             {
-                posts.map(post => <PostItem post={post} key={post.id}/>)
+                posts.map((post, index) =>
+                    <PostItem number={index + 1} post={post} key={post.id}
+                    />)
             }
         </div>
     );
