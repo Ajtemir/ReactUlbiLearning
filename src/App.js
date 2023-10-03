a9c08c9077e2b32d93c3deec8d44a8696f2c3d46
aulanbekuulu <aulanbekuulu@srs.lan>
2023-10-03 16:01:02 +0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
@@ -9,6 +9,7 @@ import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 function App() {
     const [posts, setPosts] = useState([]);
 
@@ -16,9 +17,19 @@ function App() {
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+    useEffect(()  => {
+        fetchPosts()
+    }, [])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
+    }
+
+    async function fetchPosts(){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        // console.log(response.data)
+        setPosts(response.data)
     }
 
     // Получаем post from descendant-component
@@ -28,6 +39,7 @@ function App() {
 
   return (
     <div className="App">
+        <button onClick={fetchPosts}>Get Posts</button>
         <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
             Создать пользователя
         </MyButton>
