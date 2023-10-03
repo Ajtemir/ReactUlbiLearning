c1c7c67d6252274ce70d0857aef9fd3bb879374e
aulanbekuulu <aulanbekuulu@srs.lan>
2023-10-03 17:33:50 +0600

    01:49:25 ➝ Кастомный хук useFetching(). Обработка ошибок

--- src/App.js
+++ src/App.js
@@ -12,13 +12,17 @@ import {usePosts} from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query:'',})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [isPostLoading, setIsPostsLoading] = useState(false)
-
+    // const [isPostLoading, setIsPostsLoading] = useState(false)
+    const [fetchPosts, isPostLoading, postError] = useFetching( async () => {
+        const posts = await PostService.getAll()
+        setPosts(posts)
+    })
     useEffect(()  => {
         fetchPosts()
     }, [])
@@ -26,16 +30,6 @@ function App() {
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
-    }
-
-    async function fetchPosts(){
-        setIsPostsLoading(true)
-        setTimeout(async () => {
-                const posts = await PostService.getAll()
-                setPosts(posts)
-                setIsPostsLoading(false)
-            }, 1000)
-
     }
 
     // Получаем post from descendant-component
@@ -55,6 +49,10 @@ function App() {
 
         <hr style={{margin: '15px 0'}}/>
         <PostFilter filter={filter} setFilter={setFilter} />
+        {
+            postError &&
+                <h1>Произошла ошибка ${postError}</h1>
+        }
         {
             isPostLoading
             ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
