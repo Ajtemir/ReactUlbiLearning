38592c313c1ef8da96005fc01b8b2951ae3191c7
aulanbekuulu <aulanbekuulu@srs.lan>
2023-10-04 16:59:25 +0600

    02:06:20 ➝ Обьяснение механизма изменения состояния

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
 import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query:'',})
@@ -23,7 +24,6 @@ function App() {
     const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-    let pagesArray = getPagesArray(totalPages)
 
     const [fetchPosts, isPostLoading, postError] = useFetching( async (limit, page) => {
         const response = await PostService.getAll(limit, page)
@@ -75,18 +75,13 @@ function App() {
             ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
             : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
         }
+        <Pagination
+            page={page}
+            changePage={changePage}
+            totalPages={totalPages}
+        />
 
-        <div className="page__wrapper" style={{marginTop: 30}}>
-            {
-                pagesArray.map(p =>
-                    <span
-                        onClick={() => changePage(p)}
-                        key={p}
-                        className={page === p ? 'page page__current' : 'page'}
-                >{p}
-                </span>)
-            }
-        </div>
+
 
     </div>
   );
